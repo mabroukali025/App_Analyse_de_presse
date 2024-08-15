8201dd32d289ae58ad10ec18551b53e71d2e931f
Mabrouk Ali <mabroukali025@gmail.com>
2024-08-15 11:34:26 +0100

    Initial commit

---
+++ Analyse_de_presse/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for Analyse_de_presse project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Analyse_de_presse.settings')
+
+application = get_wsgi_application()
